3f1ed48f608910b9f5295e074b17ebaa4d3876ff
Sean Hellum <seanhellum45@gmail.com>
2019-11-08 04:38:39 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rand::Rng;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess A Number!");
+    let secret_key = rand::thread_rng().gen_range(1, 101);
+    print!("What is your guess?: ");
+    let mut guess1 = String::new();
+    std::io::stdin()
+        .read_line(&mut guess1)
+        .expect("Please Enter an int");
+    let guess: i32 = guess1.trim().parse().unwrap();
+    if guess > secret_key {
+        println!("Too High!")
+    } else if guess < secret_key {
+        println!("Too Low!");
+    } else if guess == secret_key {
+        println!("You Win!");
+    }
 }
